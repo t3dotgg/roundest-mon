677ff39f3bdc9b42a35a97eadabd4a2193b7ab98
theo <me@t3.gg>
2021-11-24 17:34:06 -0800

    Fix persistance

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -24,7 +24,8 @@ export const appRouter = trpc
     async resolve({ input }) {
       const voteInDb = await prisma.vote.create({
         data: {
-          ...input,
+          votedAgainstId: input.votedAgainst,
+          votedForId: input.votedFor,
         },
       });
       return { success: true, vote: voteInDb };
