02981f19124abec98ee57190391626dc39bf23c7
theo <me@t3.gg>
2021-11-24 18:01:58 -0800

    Add link to results page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import type React from "react";
 import { inferQueryResponse } from "./api/trpc/[trpc]";
 
 import Image from "next/image";
+import Link from "next/link";
 
 const btn =
   "inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
@@ -54,6 +55,10 @@ export default function Home() {
       </div>
       <div className="absolute bottom-0 w-full text-xl text-center pb-2">
         <a href="https://github.com/TheoBr/roundest-mon">Github</a>
+        {" | "}
+        <Link href="/results">
+          <a>Results</a>
+        </Link>
       </div>
     </div>
   );
