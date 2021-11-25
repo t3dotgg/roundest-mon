1c116005f5a80bb728d222ed8a2b0d222efecce6
theo <me@t3.gg>
2021-11-24 17:06:50 -0800

    better naming

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -9,9 +9,9 @@ export const appRouter = trpc
   .query("get-pokemon-by-id", {
     input: z.object({ id: z.number() }),
     async resolve({ input }) {
-      const api = new PokemonClient();
+      const pokeApiConnection = new PokemonClient();
 
-      const pokemon = await api.getPokemonById(input.id);
+      const pokemon = await pokeApiConnection.getPokemonById(input.id);
       return { name: pokemon.name, sprites: pokemon.sprites };
     },
   })
