c936a51cdafe4512c8f329df056ce5525c17921a
theo <me@t3.gg>
2022-01-03 02:46:06 -0800

    increase cache ttl for images

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ const { withPlausibleProxy } = require("next-plausible");
 module.exports = withPlausibleProxy()({
   images: {
     domains: ["raw.githubusercontent.com"],
+    minimumCacheTTL: 6000000,
   },
 });
