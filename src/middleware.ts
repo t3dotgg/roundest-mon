b5ddae3210cce39ba827ce58b987245b84d29c88
Theo Browne <me@t3.gg>
2022-08-24 16:15:56 -0700

    added upstash redis based rate limiter

---
+++ src/middleware.ts
@@ -0,0 +1,36 @@
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.fixedWindow(20, "10 s"),
+});
+
+export default async function middleware(
+  request: NextRequest,
+  event: NextFetchEvent
+): Promise<Response | undefined> {
+  console.log("are we redis-in?");
+  const ip = request.ip ?? "127.0.0.1";
+
+  const { success, pending, limit, reset, remaining } = await ratelimit.limit(
+    `mw_${ip}`
+  );
+  event.waitUntil(pending);
+
+  console.log("success?", success);
+
+  const res = success
+    ? NextResponse.next(request)
+    : NextResponse.rewrite(new URL("/api/blocked", request.url), request);
+
+  res.headers.set("X-RateLimit-Limit", limit.toString());
+  res.headers.set("X-RateLimit-Remaining", remaining.toString());
+  res.headers.set("X-RateLimit-Reset", reset.toString());
+  return res;
+}
+
+export const config = {
+  matcher: "/api/:path*",
+};
